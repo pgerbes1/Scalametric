069ab224bd574392c4fc1901c35eb78446abc592
Patrick Gerbes <patrick@expapp.com>
2016-03-07 17:17:25 -0500

    Fix typing for proper concrete implementation

--- app/com/scalametrics/models/VectorSpace.scala
+++ app/com/scalametrics/models/VectorSpace.scala
@@ -2,17 +2,18 @@ package com.scalametrics.models
 
 import scala.languageFeature.higherKinds
 
-trait VectorSpace[A, B] {
-	implicit def field: Field[A]
-	def scalar(s: A, v: B): B
+trait VectorSpace[A, B[_]] {
+	implicit def field: Field[B[A]]
+	def scalar(s: A, v: B[A]): B[A]
 }
 object VectorSpace {
-	def scalar[A, B](s: A, r: B)(implicit vs: VectorSpace[A, B]): B = vs.scalar(s, r)
-	def apply[A, B](scalarFunction: (A, B) => B)(implicit fld: Field[A]) = new VectorSpace[A, B] {
+	def scalar[A, B[_]](s: A, r: B[A])(implicit vs: VectorSpace[A, B]): B[A] = vs.scalar(s, r)
+	def apply[A, B[_]](scalarFunction: (A, B[A]) => B[A])(implicit fld: Field[B[A]]) = new VectorSpace[A, B] {
 		def field = fld
-		def scalar(s: A, v: B) = if (!field.isEmpty(s)) scalarFunction(s, v) else v
+		def scalar(s: A, v: B[A]) = if (!field.isEmpty(v)) scalarFunction(s, v) else v
 	}
 	implicit def indexedSpace[A: Field] = apply[A, IndexedSeq[A]] {
 		(s, seq) => seq.map(Field.times(s, _))
 	}
 }
+
