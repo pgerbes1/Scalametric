1503b1fb8bf275bdb1c356d62c8c5e0cc419862f
Patrick Gerbes <patrick@expapp.com>
2016-04-12 13:24:33 -0400

    Define splat in terms of ap

--- app/com/scalametrics/models/algebra/Applicative.scala
+++ app/com/scalametrics/models/algebra/Applicative.scala
@@ -2,19 +2,23 @@ package  com.scalametrics.models.algebra
 
 trait Applicative[M[_]] extends Functor[M] {
 	def pure[A](v: A): M[A]
-	def splat[A, B](a: M[A])(f: M[A => B]): M[B]
+	def ap[A, B](a: M[A])(f: M[A => B]): M[B]
+	def splat[A, B, C](a: M[A], b: M[B])(f: (A, B) => C): M[C] = ap(b)(ap(a)(pure(f.curried)))
 }
 object Applicative {
 	def apply[M[_] : Applicative]: Applicative[M] = implicitly
 	def pure[A, M[_] : Applicative](v: A): M[A] = implicitly[Applicative[M]].pure(v)
-	def splat[M[_] : Applicative, A, B](a: M[A])(f: M[A => B]): M[B] = implicitly[Applicative[M]].splat(a)(f)
+	def ap[M[_] : Applicative, A, B](a: M[A])(f: M[A => B]): M[B] = implicitly[Applicative[M]].ap(a)(f)
+	def splat[M[_], A, B, C](a: M[A], b: M[B])(f: (A, B) => C)(implicit applic: Applicative[M]): M[C] = {
+		applic.ap(b)(applic.ap(a)(applic.pure(f.curried)))
+	}
 
 	  implicit val listApplicative: Applicative[List] = new Applicative[List] {
 		def pure[A](v: A): List[A] = List(v)
 		def fmap[A, B](l: List[A])(f: A => B): List[B] = {
 			l.map(f(_))
 		}
-		def splat[A, B](l: List[A])(f: List[A => B]): List[B] = {
+		def ap[A, B](l: List[A])(f: List[A => B]): List[B] = {
 			l.flatMap { a =>
 				f.map(b => b(a))
 				}
@@ -29,14 +33,17 @@ object Applicative {
 				case Some(_) => Option(f(o.get))
 				case None => None
 			}
-			def splat[A, B](o: Option[A])(f: Option[A => B]): Option[B] = o match {
+			def ap[A, B](o: Option[A])(f: Option[A => B]): Option[B] = o match {
 				case Some(_) => Option(f.get(o.get))
 				case None => None
 			}
 		}
 		implicit class ApplicativeOps[A, M[_] : Applicative](a: M[A]) {
 			def pure(v: A): M[A] = implicitly[Applicative[M]].pure(v)
-			def splat[B](f: M[A => B]): M[B] = implicitly[Applicative[M]].splat(a)(f)
+			def ap[B](f: M[A => B]): M[B] = implicitly[Applicative[M]].ap(a)(f)
+			def splat[B, C](b: M[B])(f: (A, B) => C)(implicit applic: Applicative[M]) = {
+				applic.ap(b)(applic.ap(a)(applic.pure(f.curried)))
+			}
 		}
 	}
 
--- app/com/scalametrics/models/algebra/Monad.scala
+++ app/com/scalametrics/models/algebra/Monad.scala
@@ -4,7 +4,7 @@ trait Monad[M[_]] extends Applicative[M] {
 	override def pure[A](v: A): M[A]
 	def flatMap[A, B](m: M[A])(f: A => M[B]): M[B]
 	override def fmap[A, B](m: M[A])(f: A => B): M[B] = flatMap(m)(a => pure(f(a)))
-	override def splat[A, B](m: M[A])(f: M[A => B]) = flatMap(m)(a => fmap(f)(b => b(a)))
+	override def ap[A, B](m: M[A])(f: M[A => B]) = flatMap(m)(a => fmap(f)(b => b(a)))
 	def flatten[A](m: M[M[A]]): M[A] = flatMap(m)(m => m)
 	def compose[A, B, C](f1: A => M[B])(f2: B => M[C]): A => M[C] = a => flatMap(f1(a))(f2)
 	def join[A, B, C](m1: M[A])(m2: M[B])(f: (A, B) => C): M[C] = flatMap(m1)(a => fmap(m2)(b => f(a, b)))
@@ -29,7 +29,7 @@ object Monad {
   implicit class MonadOps[A, M[_] : Monad](m: M[A]) {
 	  def pure(v: A): M[A] = implicitly[Monad[M]].pure(v)
 	  def fmap[B](f: A => B): M[B] = implicitly[Monad[M]].fmap(m)(f)
-	  def splat[B](f: M[A => B]): M[B] = implicitly[Monad[M]].splat(m)(f)
+	  def ap[B](f: M[A => B]): M[B] = implicitly[Monad[M]].ap(m)(f)
 		def flatMap[B](f: A => M[B]): M[B] = implicitly[Monad[M]].flatMap(m)(f)
 		def flatten(m: M[M[A]]): M[A] = implicitly[Monad[M]].flatten(m)
 	  def join[B, C](m2: M[B])(f: (A, B) => C): M[C] = implicitly[Monad[M]].join(m)(m2)(f)
